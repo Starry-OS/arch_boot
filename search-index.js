94e77a933bdf7c32213317e677521963c4959ba2
Azure-stars <Azure_stars@126.com>
2024-11-10 09:51:13 +0000

    Deploying to gh-pages from @ Starry-OS/arch_boot@dc98dfc8e6374dead21a78bc982e898595da6284 🚀

---
+++ search-index.js
@@ -0,0 +1,5 @@
+var searchIndex = new Map(JSON.parse('[\
+["arch_boot",{"t":"H","n":["current_boot_stack"],"q":[[0,"arch_boot"]],"i":[0],"f":"{{}b}","D":"d","p":[[1,"u8"]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OjAAAAAAAAA="}]\
+]'));
+if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
+else if (window.initSearch) window.initSearch(searchIndex);
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = new Map(JSON.parse('[\
+["arch_boot",["",[["platform",[["dummy",[],["mod.rs"]]],["mod.rs"]]],["lib.rs","mp.rs"]]]\
+]'));
+createSrcSidebar();
